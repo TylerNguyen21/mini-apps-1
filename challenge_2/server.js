57ddccd73369b8b2b2a93df300b6a6d3bb349c11
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-12-11 12:10:10 -0800

    set up index.html file and server.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000
 
+app.use(express.static('client'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('hello world');
+})
+
+app.post('/', function(req, res) {
+    console.log(req);
+    res.send()
+})
+
+app.listen(port, function() {
+    console.log(`Dev app listening on ${port}!`);
+})
 
