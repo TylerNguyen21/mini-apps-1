ef17b916730c7a121715ac048b16a96de873b5e6
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-12-11 20:19:28 -0800

    Update app.js

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -8,6 +8,7 @@ let template = `<!DOCTYPE html>
   src="https://code.jquery.com/jquery-3.3.1.js"
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
   crossorigin="anonymous"></script>
+    <link rel = "stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <h1>CSV REPORT GENERATOR</h1>
