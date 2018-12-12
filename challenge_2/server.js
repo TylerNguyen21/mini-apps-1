4abf2a243dad2813dd257a4a86e4610c335b00cc
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-12-11 21:18:48 -0800

    implemented file chooser

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
 const converter = require('./client/app.js')
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
@@ -12,34 +15,41 @@ app.get('/', function(req, res) {
     res.send('hello world');
 })
 
-app.post('/', function(req, res) {
-    let parseObj = JSON.parse(req.body.json);
-    let fileArray = [];
-    const iterateThroughFile = (obj) => {
-        let storage = [];
-        for (var key in obj) {
-            if (key === 'children') {
-                continue;
+app.post('/', upload.single('fileUpload'), function(req, res) {
+    fs.readFile(req.file.path, (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            let parseObj = JSON.parse(data);
+            let fileArray = [];
+            const iterateThroughFile = (obj) => {
+                let storage = [];
+                for (var key in obj) {
+                    if (key === 'children') {
+                        continue;
+                    }
+                    storage.push(obj[key]);
+                }
+                fileArray.push(storage);
+                if(obj.children.length > 0) {
+                    for (var x = 0; x < obj.children.length; x++) {
+                        iterateThroughFile(obj.children[x])
+                    }
+                }
+                
             }
-            storage.push(obj[key]);
+            iterateThroughFile(parseObj);
+            
+            converter.csvFileConverter(fileArray, (err, result) => {
+              if (err) {
+                  throw err;
+              } else {
+                res.send(result);   
+              }
+            });
         }
-        fileArray.push(storage);
-        if(obj.children.length > 0) {
-            for (var x = 0; x < obj.children.length; x++) {
-                iterateThroughFile(obj.children[x])
-            }
-        }
-        
-    }
-    iterateThroughFile(parseObj);
-    
-    converter.csvFileConverter(fileArray, (err, result) => {
-      if (err) {
-          throw err;
-      } else {
-        res.send(result);   
-      }
-    });
+    })
+   
 })
 
 app.listen(port, function() {
